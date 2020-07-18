c82410881f4b07cc41bd174ea65ef8ef0d1f0342
NicholasCF <nicholas.fernando@u.nus.edu>
2020-07-18 21:32:19 +0800

    Configure heroku settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn meridien.wsgi
--- meridien/settings/deployment.py
+++ meridien/settings/deployment.py
@@ -1,6 +1,13 @@
 import os
 
+import django_heroku
+import dj_database_url
+
 from meridien.settings.base import *
+
+# Static files
+
+STATIC_ROOT = 'static'
 
 # Security
 
@@ -11,16 +18,8 @@ SECRET_KEY = os.getenv('MERIDIEN_SECRET_KEY')
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('MERIDIEN_DJANGO_DB_NAME'),
-        'USER': os.getenv('MERIDIEN_DJANGO_DB_USER'),
-        'PASSWORD': os.getenv('MERIDIEN_DJANGO_DB_PASSWORD'),
-        'HOST': os.getenv('MERIDIEN_DJANGO_DB_HOST'),
-        'PORT': 5432,
-    }
-}
+DATABASES = dict()
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # CORS settings
 CORS_ORIGIN_ALLOW_ALL = False
@@ -51,3 +50,6 @@ SECURE_REFERRER_POLICY = 'same-origin'
 SECURE_SSL_REDIRECT = True
 
 SESSION_COOKIE_SECURE = True
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
